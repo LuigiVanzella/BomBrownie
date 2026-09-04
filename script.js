c1435a4c1a21a4432ee740189e1c2b4d692776cf
Luigi Antonio Loddi Vanzella <111365562+LuigiVanzella@users.noreply.github.com>
2026-09-04 16:46:33 -0300

    Criar script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Adiciona um fundo dinâmico ao cabeçalho ao rolar a página
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
